a6506ed2dac5270ad0add05cb569969aaaa44e38
harsh2301-code <harshchoudhury40@gmail.com>
2026-06-13 15:22:31 +0530

    Dy 11 assignment is uploaded

---
+++ Day 11/program to Write function to find maximum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int maximum(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Maximum = %d", maximum(a, b));
+
+    return 0;
+}
